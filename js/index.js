e66ed21981a0b16752c4a50cc81a733b29ccd895
Osorabrian <osorabrian@gmail.com>
2022-12-14 15:38:42 +0300

    added sign up button

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const logInDiv = document.getElementById('logindiv')
+    const logInForm = document.getElementById('loginform')
+
+    logInForm.addEventListener('submit', () => {
+        logInDiv.style.display = 'none'
+})
+})
